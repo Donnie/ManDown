3f7ee48b3230e5ee452f009613e36f798d51ff91
Donnie <donnie.ashok@moonfare.com>
2023-08-27 12:00:48 +0200

    v2.0 - Add Teloxide

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,46 @@ mod http;
 use std::path::Path;
 use dotenv::dotenv;
 
+use teloxide::{prelude::*, utils::command::BotCommands};
+use teloxide::repls::CommandReplExt;
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "I am here to help!")]
+    Help,
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => bot.send_message(
+            msg.chat.id,
+            Command::descriptions().to_string(),
+        ).await?,
+    };
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    
+
     let filename = dotenv::var("DBFILE").unwrap_or("db/db.csv".to_string());
     let interval: u64 = dotenv::var("FREQ")
         .unwrap_or("600".to_string())
         .parse()
-        .expect("Interval must be a number");
+        .expect("FREQ must be a number");
 
     // Check that the file exists
     if !Path::new(&filename).exists() {
-        println!("The file {} does not exist", filename);
-        return;
+        panic!("The DBFILE {} does not exist", filename);
     }
 
-    run_poll(filename, interval).await;
+    // Start polling function
+    tokio::spawn(async move {
+        run_poll(filename, interval).await;
+    });
+
+    let bot = Bot::from_env();
+    Command::repl(bot, answer).await;
 }
