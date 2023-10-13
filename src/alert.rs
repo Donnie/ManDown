50fdf5476c8d6dbcb85af78e4337d1533c45008b
Donnie <donnie.ashok@moonfare.com>
2023-10-13 09:22:37 +0200

    v2.0 - Workflow Dispatch

--- src/alert.rs
+++ src/alert.rs
@@ -7,15 +7,15 @@ pub async fn notify_user(conn: &mut SqliteConnection, bot: Bot, changed_webs: Ve
     for website in changed_webs {
         // Look up users for this website
         let users = list_users_by_website(conn, website.id)
-        .await
-        .expect("Error getting Users");
-        
+            .await
+            .expect("Error getting Users");
+
         // Notify each user
         for user in users {
             let chat_id = ChatId(user.telegram_id as i64);
-            
+
             let message = process(&website.url, website.status);
-            
+
             bot.send_message(chat_id, message)
                 .parse_mode(ParseMode::Html)
                 .await
@@ -26,7 +26,7 @@ pub async fn notify_user(conn: &mut SqliteConnection, bot: Bot, changed_webs: Ve
 
 pub fn process(site: &str, code: i32) -> String {
     let mut output = format!("Site: {}\n\n", site);
-    
+
     match code {
         0 | 1 => output += &format!("Hoppla! We faced an error trying to reach the site! 🤒"),
         200..=299 => output += &format!("Joohoo! It's live and kicking! 🙂\n\nStatus: <a href='https://httpstatuses.com/{}'>{}</a>", code, code),
@@ -34,6 +34,6 @@ pub fn process(site: &str, code: i32) -> String {
         500..=599 => output += &format!("Schade! It's down or inaccessible to me! 😟\n\nStatus: <a href='https://httpstatuses.com/{}'>{}</a>", code, code),
         _ => output += "Something is fishy! 🐟",
     }
-    
+
     output
 }
