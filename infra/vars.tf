a611a447765e6afd584a552d5ef530174af25a3c
Donnie Ashok <git@donnie.in>
2025-06-09 18:24:25 +0200

    Add env variables (#45)
    * Add env variables
    
    * Add freq

--- infra/vars.tf
+++ infra/vars.tf
@@ -3,11 +3,6 @@ variable "app_name" {
   type        = string
 }
 
-variable "image" {
-  description = "Docker image"
-  type        = string
-}
-
 variable "project_id" {
   description = "GCP Project ID"
   type        = string
@@ -21,4 +16,28 @@ variable "region" {
 variable "zone" {
   description = "GCP Zone"
   type        = string
+}
+
+## Compute Engine
+variable "freq" {
+  description = "Frequency in seconds"
+  type        = string
+  default     = "600"
+}
+
+variable "image" {
+  description = "Docker image"
+  type        = string
+}
+
+variable "teloxide_token" {
+  description = "Teloxide token"
+  type        = string
+  sensitive   = true
+}
+
+variable "mongodb_uri" {
+  description = "MongoDB URI"
+  type        = string
+  sensitive   = true
 }
